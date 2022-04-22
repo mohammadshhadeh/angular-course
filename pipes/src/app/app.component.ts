97e0b3b4434cad2ee924092b5851ae0afab7daab
mohammadshhadeh <mohammadzaidsh@outlook.com>
2022-04-22 20:08:15 +0300

    pipes

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+interface Server {
+  instanceType: string;
+  name: string;
+  status: string;
+  started: Date;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017),
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    },
+  ];
+  getStatusClasses(server: Server) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical',
+    };
+  }
+}
