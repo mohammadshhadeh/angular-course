a84c171dde0c30405795765f73fd849f2cb84bbf
mohammadshhadeh <mohammadzaidsh@outlook.com>
2022-04-23 01:44:54 +0300

    more interceptors

--- http-request/src/app/auth-interceptor.service.ts
+++ http-request/src/app/auth-interceptor.service.ts
@@ -12,14 +12,9 @@ export class AuthInterceptorService implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const modifiedRequest = req.clone({ headers: req.headers.append('Auth', 'xyz') });
-    console.log('Request is on its way');
-    return next.handle(modifiedRequest).pipe(tap(event => {
-      console.log(event)
-      if (event.type === HttpEventType.Response) {
-        console.log('Response arrived, body data: ');
-        console.log(event.body);
-      }
-    }));
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'xyz'),
+    });
+    return next.handle(modifiedRequest);
   }
 }
