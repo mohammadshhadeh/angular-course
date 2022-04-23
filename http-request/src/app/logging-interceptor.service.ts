1d776f1a7b48b6a7f20f93e72178af292390c633
mohammadshhadeh <mohammadzaidsh@outlook.com>
2022-04-23 15:54:58 +0300

    more interceptors

---
+++ http-request/src/app/logging-interceptor.service.ts
@@ -0,0 +1,24 @@
+import {
+  HttpEvent,
+  HttpEventType,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+export class LoggingInterceptorService implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    console.log('Outgoing request');
+    console.log(req.url);
+    return next.handle(req).pipe(tap(event => {
+      if (event.type === HttpEventType.Response) {
+        console.log('Response arrived, body data:');
+        console.log(event.body);
+      }
+    }));
+  }
+}
