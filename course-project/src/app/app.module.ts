f536eec4bd63fee2acdbfff715f542d0538ac747
mohammadshhadeh <mohammadzaidsh@outlook.com>
2022-04-29 23:15:56 +0300

    shared module

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -6,26 +6,19 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingService } from './components/shopping-list/shopping.service';
 import { RecipeService } from './components/recipes/recipe.service';
 import { AuthComponent } from './components/auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './components/auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
-import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipesModule } from './components/recipes/recipes.module';
 import { ShoppingListModule } from './components/shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		AuthComponent,
-		DropdownDirective,
 		HeaderComponent,
-		LoadingSpinnerComponent,
-		AlertComponent,
-		PlaceholderDirective,
 	],
 	imports: [
 		AppRoutingModule,
@@ -35,6 +28,7 @@ import { ShoppingListModule } from './components/shopping-list/shopping-list.mod
 		ReactiveFormsModule,
 		RecipesModule,
 		ShoppingListModule,
+		SharedModule,
 	],
 	providers: [
 		RecipeService,
