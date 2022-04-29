88e3b331f276a376c688ee3a2440712ea7825f29
mohammadshhadeh <mohammadzaidsh@outlook.com>
2022-04-30 00:10:29 +0300

    lazy load

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -3,6 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 	{ path: '', redirectTo: '/recipes', pathMatch: 'full' },
+	{
+		path: 'recipes',
+		loadChildren: () =>
+			import('./components/recipes/recipes.module').then(
+				(x) => x.RecipesModule
+			),
+	},
+	{
+		path: 'auth',
+		loadChildren: () =>
+			import('./components/auth/auth.module').then(
+				(x) => x.AuthModule
+			),
+	},
+	{
+		path: 'ShoppingListModule',
+		loadChildren: () =>
+			import('./components/shopping-list/shopping-list.module').then(
+				(x) => x.ShoppingListModule
+			),
+	},
 ];
 
 @NgModule({
--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { RecipesModule } from './components/recipes/recipes.module';
-import { ShoppingListModule } from './components/shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './components/auth/auth.module';
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent],
@@ -19,10 +15,7 @@ import { AuthModule } from './components/auth/auth.module';
 		BrowserModule,
 		CoreModule,
 		HttpClientModule,
-		RecipesModule,
 		SharedModule,
-		ShoppingListModule,
-		AuthModule,
 	],
 	bootstrap: [AppComponent],
 	// old version of ng
